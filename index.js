1d4a8f850402d686e0cf2c40457b13f882f5bd19
Lucas Tadeu <lucastgfp@gmail.com>
2024-03-04 18:50:45 -0300

    Final

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ import Evento from "./Modelos/Evento.js";
 
 // EVENTOS
 
-//const evento = new Evento(0, "Banda THUNDER", "20h00", "20/03 - Quarta-Feira", "Teatro PP1", "2 horas", "Livre", "50", "100");
-//const evento = new Evento(0, "Banda EARTH", "19h00", "21/03 - Quinta-Feira", "Teatro PP2", "1 hora", "Livre", "40", "90");
-//const evento = new Evento(5, "Banda ICE", "20h00", "22/03 - Sexta-Feira", "Teatro PP1", "30 minutos", "Livre", "55", "120");
-
-//console.log(evento.toJSON( ));
-
+//const evento = new Evento(0, "Banda THUNDER", "18h00", "20/03 - Quarta-Feira", "Teatro PP1", "2 horas", "Livre", "50", "100");
+//const evento = new Evento(1, "Banda EARTH", "19h00", "21/03 - Quinta-Feira", "Teatro PP2", "1 hora", "Livre", "40", "90");
+//const evento = new Evento(2, "Banda ICE", "20h00", "22/03 - Sexta-Feira", "Teatro PP1", "45 minutos", "Livre", "55", "110");
+//const evento = new Evento(3, "Banda ICE", "21h00", "23/03 - Sábado", "Teatro PP2", "45 minutos", "Livre", "60", "125");
 
 // GRAVAR, ATUALIZAR E EXCLUIR
 
